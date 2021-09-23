f858d209cad0fc11cad660006f5af10c29a51e59
John Arkema <john.arkema@gmail.com>
2021-09-23 09:19:51 -0700

    add build script to package

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import DisplayClocks from './views/DisplayClocks';
 const App = () => {
 	return (
 		<div className='mx-auto text-center bg-themeBlack'>
-			<h1 className='text-3xl'>Dashboard</h1>
+			<h1 className='text-3xl text-white pt-4'>Dashboard</h1>
 			<div className='container mx-auto grid gap-8 mt-10 md:grid-cols-2 lg:grid-cols-3'>
 				<DisplayWeather />
 				<DisplayPWG />
